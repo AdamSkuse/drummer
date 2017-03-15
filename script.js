7afb280b1622db7349a1aba781144fb0e85eb296
AdamSkuse <a.t.skuse@gmail.com>
2017-03-15 19:26:22 +0000

    Adds transitionend listener (with help from tutorial)

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
-document.body.addEventListener('keydown', function(event) { parseKeyDown(event) } );
+document.body.addEventListener('keydown', function(event) { playSound(event) } );
+var drumKeys = document.querySelectorAll('.drum');
+drumKeys.forEach(key => key.addEventListener('transitionend', removeTransition));
 
 var validKeys = ['a','s','d','f','g','h','j','k','l'];
 
-function parseKeyDown(e) {
+function removeTransition(e) {
+    this.classList.remove('playing');
+}
+
+function playSound(e) {
     var key = e.key;
     if (validKeys.includes(key)) {
-        document.getElementById('drum-' + key).play();
+        var sound = document.getElementById('drum-' + key);
+        sound.currentTime = 0;
+        sound.play();
+        var div = document.getElementById(key);
+        div.classList.add('playing');
     }
 }
 
